6c7b8c6963481d19c1ec77385c7860abb8d5dadf
Alexandrshy <alexandrshy@gmail.com>
2019-01-10 02:14:47 +0400

    Fixed layout

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,36 +8,39 @@
       >Want to grow as a developer and find great work? Do not wipe your pants - engage Open Source projects. So the easiest way to get into the best development teams and put yourself in the summary of this project.</p>
       <button class="button">Help</button>
       <div class="task">
-        <div class="task__wrapper">
-          <div class="row">
-            <div class="col-sm-3">
-              <div class="task__switch">
-                <button>Open task</button>
-                <button>Closed task</button>
+        <div class="task__wrapper row">
+          <ul class="task__switch-list">
+            <li class="task__switch-item">
+              <button class="task__switch-button is-active">Open task</button>
+            </li>
+            <li class="task__switch-item">
+              <button class="task__switch-button">Closed task</button>
+            </li>
+          </ul>
+          <ul class="task__list">
+            <li class="task__item" v-for="task in tasks" :key="task.id">
+              <div class="task__logo-part">
+                <img class="task__logo" :src="`${task.projectLogo}`" :alt="`${task.project} logo`">
               </div>
-            </div>
-            <div class="col-sm-9">
-              <ul class="task__list">
-                <li class="task__item" v-for="task in tasks" :key="task.id">
-                  <div class="task__logo-part">
-                    <img
-                      class="task__logo"
-                      :src="`${task.projectLogo}`"
-                      :alt="`${task.project} logo`"
-                    >
-                  </div>
-                  <div class="task__desc-part">
-                    <div class="task__item-header">
-                      <a class="task__project-title" href="`${task.projectLink}`">{{task.project}}</a>
-                      <a class="task__author" href="`${task.authorLink}`">{{task.author}}</a>
-                    </div>
-                    <div class="task__item-main"></div>
-                    <div class="task__item-footer"></div>
-                  </div>
-                </li>
-              </ul>
-            </div>
-          </div>
+              <div class="task__desc-part">
+                <div class="task__item-header">
+                  <a
+                    class="task__header-link task__project-title"
+                    href="`${task.projectLink}`"
+                  >{{task.project}}</a>
+                  <a
+                    class="task__header-link task__author"
+                    href="`${task.authorLink}`"
+                  >{{task.author}}</a>
+                </div>
+                <div class="task__item-main">
+                  <h3 class="task__title">{{task.title}}</h3>
+                  <p class="task__description">{{task.description}}</p>
+                </div>
+                <div class="task__item-footer"></div>
+              </div>
+            </li>
+          </ul>
         </div>
       </div>
     </section>
@@ -63,6 +66,21 @@ export default {
           language: "JavaScript",
           level: "junior",
           completed: false
+        },
+        {
+          id: 2,
+          author: "Alex Shu",
+          authorLink: "https://github.com/Alexandrshy",
+          project: "OpenHelpList",
+          projectLink: "https://github.com/Alexandrshy/OpenHelpList",
+          projectLogo:
+            "https://user-images.githubusercontent.com/10379601/29446482-04f7036a-841f-11e7-9872-91d1fc2ea683.png",
+          title: "Make the first commit",
+          description:
+            "Make the first commit Make the first commit Make the first commit Make the first commit Make the first commit Make the first commit Make the first commit Make the first commit Make the first commit Make the first commit Make the first commit Make the first commit Make the first commit Make the first commit Make the first commit Make the first commit Make the first commit Make the first commit Make the first commit Make the first commit Make the first commit",
+          language: "JavaScript",
+          level: "junior",
+          completed: true
         }
       ]
     };
