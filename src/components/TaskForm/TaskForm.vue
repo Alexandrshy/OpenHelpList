116bfcca9858287833bd09763044eaad5f4ba816
Alexandrshy <alexandrshy@gmail.com>
2019-01-23 01:26:43 +0400

    started working on the form component

--- src/components/TaskForm/TaskForm.vue
+++ src/components/TaskForm/TaskForm.vue
@@ -4,24 +4,34 @@
       <form class="task-form__elem">
         <div class="task-form__item">
           <label for="taskTitle">Task title</label>
-          <input id="taskTitle" type="text" placeholder="Example: ">
+          <input
+            id="taskTitle"
+            type="text"
+            placeholder="Example: Write a unit test for a form component"
+          >
         </div>
         <div class="task-form__item">
           <label for="taskLink">Link to task</label>
-          <input id="taskLink" type="text" placeholder="Example: ">
+          <input
+            id="taskLink"
+            type="text"
+            placeholder="Example: https://github.com/Author/Project/issues/1"
+          >
         </div>
         <div class="task-form__item">
           <label for="taskDesc">Task description</label>
-          <textarea
-            id="taskDesc"
-            rows="16"
-            class="form-control"
-            v-model="md_text"
-            placeholder="Example: "
-          ></textarea>
+          <textarea id="taskDesc" rows="16" class="form-control" v-model="md_text"></textarea>
           <div class="well well-sm pre-scrollable" v-html="previewText"></div>
         </div>
         <div class="task-form__item">
+          <label for="taskLink">Link to task</label>
+          <input
+            id="taskLink"
+            type="text"
+            placeholder="Example: https://github.com/Author/Project/issues/1"
+          >
+        </div>
+        <div class="task-form__item task-form__item--button">
           <button class="button">Send</button>
         </div>
       </form>
